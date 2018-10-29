2919f2baf2ae17b0420601cbf77571ff1d8de60c
dedaCar <nikola89bgd@gmail.com>
2018-10-29 19:46:56 +0100

    third commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const { startCoordinate, endCoordinate, blocks } = require('./config');
 
+
+const LOCATION_STATUS = {
+	INVALID: 'invalid',
+	GOAL: 'goal',
+	BLOCKED: 'blocked',
+	VALID: 'valid',
+	EMPTY: 'empty',
+	VISITED: 'visited',
+	START: 'start',
+	OBSTACLE: 'obstacle',
+	NORTH: 'north',
+	SOUTH: 'south',
+	EAST: 'east',
+	WEST: 'west',
+};
+
 function locationStatus(location, grid) {
 	const gridSize = grid.length;
 	const dft = location.distanceFromTop;
@@ -8,13 +24,13 @@ function locationStatus(location, grid) {
       || location.distanceFromLeft >= gridSize
       || location.distanceFromTop < 0
       || location.distanceFromTop >= gridSize) {
-		return 'Invalid';
-	} if (grid[dft][dfl] === 'Goal') {
-		return 'Goal';
-	} if (grid[dft][dfl] !== 'Empty') {
-		return 'Blocked';
+		return LOCATION_STATUS.INVALID;
+	} if (grid[dft][dfl] === LOCATION_STATUS.GOAL) {
+		return LOCATION_STATUS.GOAL;
+	} if (grid[dft][dfl] !== LOCATION_STATUS.EMPTY) {
+		return LOCATION_STATUS.BLOCKED;
 	}
-	return 'Valid';
+	return LOCATION_STATUS.VALID;
 }
 
 
@@ -24,13 +40,13 @@ function exploreInDirection(currentLocation, direction, grid) {
 	newPath.push(`${currentLocation.distanceFromTop}, ${currentLocation.distanceFromLeft}`);
 	let dft = currentLocation.distanceFromTop;
 	let dfl = currentLocation.distanceFromLeft;
-	if (direction === 'North') {
+	if (direction === LOCATION_STATUS.NORTH) {
 		dft -= 1;
-	} else if (direction === 'East') {
+	} else if (direction === LOCATION_STATUS.EAST) {
 		dfl += 1;
-	} else if (direction === 'South') {
+	} else if (direction === LOCATION_STATUS.SOUTH) {
 		dft += 1;
-	} else if (direction === 'West') {
+	} else if (direction === LOCATION_STATUS.WEST) {
 		dfl -= 1;
 	}
 	const newLocation = {
@@ -41,8 +57,8 @@ function exploreInDirection(currentLocation, direction, grid) {
 	};
 	newLocation.status = locationStatus(newLocation, grid);
 
-	if (newLocation.status === 'Valid') {
-		grid[newLocation.distanceFromTop][newLocation.distanceFromLeft] = 'Visited';
+	if (newLocation.status === LOCATION_STATUS.VALID) {
+		grid[newLocation.distanceFromTop][newLocation.distanceFromLeft] = LOCATION_STATUS.VISITED;
 	}
 	return newLocation;
 }
@@ -56,8 +72,8 @@ function findShortestPath(startCoordinates, grid) {
 		for (let i = 0; i < grid.length; i += 1) {
 			const row = grid[i];
 			for (let j = 0; j < row.length; j += 1) {
-				if (row[j] === 'Visited') {
-					row[j] = 'Empty';
+				if (row[j] === LOCATION_STATUS.VISITED) {
+					row[j] = LOCATION_STATUS.EMPTY;
 				}
 			}
 		}
@@ -70,7 +86,7 @@ function findShortestPath(startCoordinates, grid) {
 		distanceFromTop,
 		distanceFromLeft,
 		path: [],
-		status: 'Start',
+		status: LOCATION_STATUS.START,
 	};
 
 	const queue = [location];
@@ -78,43 +94,43 @@ function findShortestPath(startCoordinates, grid) {
 	while (queue.length > 0) {
 		const currentLocation = queue.shift();
 		// Explore North
-		const northLocation = exploreInDirection(currentLocation, 'North', grid);
-		if (northLocation.status === 'Goal') {
+		const northLocation = exploreInDirection(currentLocation, LOCATION_STATUS.NORTH, grid);
+		if (northLocation.status === LOCATION_STATUS.GOAL) {
 			const result = northLocation.path;
 			result.push(`${currentLocation.distanceFromTop - 1}, ${currentLocation.distanceFromLeft}`);
 			reset(grid);
 			return result;
-		} if (northLocation.status === 'Valid') {
+		} if (northLocation.status === LOCATION_STATUS.VALID) {
 			queue.push(northLocation);
 		}
 		// Explore East
-		const eastLocation = exploreInDirection(currentLocation, 'East', grid);
-		if (eastLocation.status === 'Goal') {
+		const eastLocation = exploreInDirection(currentLocation, LOCATION_STATUS.EAST, grid);
+		if (eastLocation.status === LOCATION_STATUS.GOAL) {
 			const result = eastLocation.path;
 			result.push(`${currentLocation.distanceFromTop}, ${currentLocation.distanceFromLeft + 1}`);
 			reset(grid);
 			return result;
-		} if (eastLocation.status === 'Valid') {
+		} if (eastLocation.status === LOCATION_STATUS.VALID) {
 			queue.push(eastLocation);
 		}
 		// Explore South
-		const southLocation = exploreInDirection(currentLocation, 'South', grid);
-		if (southLocation.status === 'Goal') {
+		const southLocation = exploreInDirection(currentLocation, LOCATION_STATUS.SOUTH, grid);
+		if (southLocation.status === LOCATION_STATUS.GOAL) {
 			const result = southLocation.path;
 			result.push(`${currentLocation.distanceFromTop + 1}, ${currentLocation.distanceFromLeft}`);
 			reset(grid);
 			return result;
-		} if (southLocation.status === 'Valid') {
+		} if (southLocation.status === LOCATION_STATUS.VALID) {
 			queue.push(southLocation);
 		}
 		// Explore West
-		const westLocation = exploreInDirection(currentLocation, 'West', grid);
-		if (westLocation.status === 'Goal') {
+		const westLocation = exploreInDirection(currentLocation, LOCATION_STATUS.WEST, grid);
+		if (westLocation.status === LOCATION_STATUS.GOAL) {
 			const result = westLocation.path;
 			result.push(`${currentLocation.distanceFromTop}, ${currentLocation.distanceFromLeft - 1}`);
 			reset(grid);
 			return result;
-		} if (westLocation.status === 'Valid') {
+		} if (westLocation.status === LOCATION_STATUS.VALID) {
 			queue.push(westLocation);
 		}
 	}
@@ -130,11 +146,11 @@ function generateRandomMatrix(size, start, end) {
 	let placedBlocks = 0;
 	const matrix = [];
 	for (let i = 0; i < size; i += 1) {
-		matrix.push(new Array(size).fill('Empty'));
+		matrix.push(new Array(size).fill(LOCATION_STATUS.EMPTY));
 	}
 
-	matrix[x1][y1] = 'Start';
-	matrix[x2][y2] = 'Goal';
+	matrix[x1][y1] = LOCATION_STATUS.START;
+	matrix[x2][y2] = LOCATION_STATUS.GOAL;
 
 	while (placedBlocks < blocks) {
 		console.log('trying to place obstacle');
@@ -142,16 +158,16 @@ function generateRandomMatrix(size, start, end) {
 		const randY = randomNumberBetween(0, 9);
 
 		const isStartOrEnd = (randX === x1 && randY === y1) || (randX === x2 && randY === y2);
-		const isAlreadyObstacle = matrix[randX][randY] === 'Obstacle';
+		const isAlreadyObstacle = matrix[randX][randY] === LOCATION_STATUS.OBSTACLE;
 
 		if (!(isStartOrEnd || isAlreadyObstacle)) {
 			console.log(`Placing on ${randX} - ${randY}`);
-			matrix[randX][randY] = 'Obstacle';
+			matrix[randX][randY] = LOCATION_STATUS.OBSTACLE;
 		}
 
 
 		if (!findShortestPath(startCoordinate, matrix)) {
-			matrix[randX][randY] = 'Empty';
+			matrix[randX][randY] = LOCATION_STATUS.EMPTY;
 		}
 		placedBlocks += 1;
 	}
